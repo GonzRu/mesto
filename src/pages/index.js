d4d8c80c2a04123745f5a54c106325df8fe9b3f9
Denis Smirnov <gonzru@gmail.com>
2022-06-22 00:01:19 +0300

    new project structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import Card from "./Card.js";
-import FormValidator from "./FormValidator.js";
-import Section from './Section.js';
-import PopupWithImage from './PopupWithImage.js';
-import PopupWithForm from './PopupWithForm.js';
-import UserInfo from "./UserInfo.js";
-import { initialCards, formConstants, cardConstants, selectors } from "./constants.js";
+import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import Section from '../components/Section.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import UserInfo from "../components/UserInfo.js";
+import { initialCards, formConstants, cardConstants, selectors } from "../utils/constants.js";
 import '../pages/index.css';
 
 // Profile
